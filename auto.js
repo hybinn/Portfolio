dba06509f9a5d20a131cca4cc72793db01b32203
김현빈 <105833285+hybinn@users.noreply.github.com>
2024-04-24 01:07:48 +0900

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,12 +1,11 @@
-// Bootstrap의 Collapse 컴포넌트를 사용하여 아코디언을 초기화합니다.
 var accordion = new bootstrap.Collapse(
   document.getElementById('accordionFlushExample'),
   {
-    toggle: false, // 수동으로 아코디언을 열고 닫을 수 있도록 toggle 옵션을 false로 설정합니다.
+    toggle: false, 
   }
 )
 
-// 각 아코디언 아이템의 버튼을 클릭할 때 아코디언을 열고 닫습니다.
+.
 document.querySelectorAll('.accordion-button').forEach(function (button) {
   button.addEventListener('click', function () {
     var target = button.getAttribute('data-bs-target')
@@ -15,9 +14,9 @@ document.querySelectorAll('.accordion-button').forEach(function (button) {
       if (collapse) {
         var isOpen = collapse.classList.contains('show')
         if (!isOpen) {
-          accordion.show(collapse) // 아코디언 열기
+          accordion.show(collapse)
         } else {
-          accordion.hide(collapse) // 아코디언 닫기
+          accordion.hide(collapse) 
         }
       }
     }
